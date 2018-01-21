6236246545ffb6303be5ded8add3c1e49ff429b2
Sergey Sova <mail@sergeysova.com>
2018-01-22 02:10:03 +0300

    Format code with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-extern crate colored;
-extern crate toml;
 #[macro_use]
 extern crate clap;
+extern crate colored;
+extern crate toml;
 
 use std::fs::File;
 use std::io::prelude::Read;
@@ -13,10 +13,10 @@ fn print_toml(value: &Value, parent_name: Option<&String>) {
     match *value {
         Value::String(ref string) => {
             print!("{}{}{}", "\"".green(), string.green(), "\"".green());
-        },
+        }
         Value::Float(ref float) => {
             print!("{}", float.to_string().purple());
-        },
+        }
         Value::Integer(ref integer) => {
             print!("{}", integer.to_string().purple());
         }
@@ -25,10 +25,10 @@ fn print_toml(value: &Value, parent_name: Option<&String>) {
                 true => print!("{}", "true".yellow()),
                 false => print!("{}", "false".yellow()),
             }
-        },
+        }
         Value::Datetime(ref datetime) => {
             print!("{}", datetime.to_string().magenta());
-        },
+        }
         Value::Array(ref array) => {
             print!("{}", "[".blue());
             for (index, val) in array.iter().enumerate() {
@@ -39,11 +39,13 @@ fn print_toml(value: &Value, parent_name: Option<&String>) {
                 }
             }
             print!("{}", "]".blue());
-        },
+        }
         Value::Table(ref table) => {
-            for (name, value) in table.iter().filter(|&(_, ref value)| value.type_str() != "table") {
+            for (name, value) in table
+                    .iter()
+                    .filter(|&(_, ref value)| value.type_str() != "table") {
                 match *value {
-                    Value::Table(..) => {},
+                    Value::Table(..) => {}
                     _ => {
                         print!("{} = ", name.blue());
                         print_toml(value, None);
@@ -51,24 +53,28 @@ fn print_toml(value: &Value, parent_name: Option<&String>) {
                     }
                 }
             }
-            for (name, value) in table.iter().filter(|&(_, ref value)| value.type_str() == "table") {
+            for (name, value) in table
+                    .iter()
+                    .filter(|&(_, ref value)| value.type_str() == "table") {
                 match *value {
                     Value::Table(ref table) => {
                         #[allow(unused_assignments)]
-                        let mut fullname = String::new();
 
-                        if let Some(parent_name) = parent_name {
-                            fullname = format!("{}.{}", parent_name, name);
-                        }
-                        else {
-                            fullname = name.clone();
-                        }
-                        if table.iter().filter(|&(_, ref value)| value.type_str() != "table").count() > 0 {
+                        let mut fullname = if let Some(parent_name) = parent_name {
+                            format!("{}.{}", parent_name, name)
+                        } else {
+                            name.clone()
+                        };
+
+                        if table
+                               .iter()
+                               .filter(|&(_, ref value)| value.type_str() != "table")
+                               .count() > 0 {
                             println!("");
                             println!("{}{}{}", "[".blue(), fullname.white(), "]".blue());
                         }
                         print_toml(value, Option::from(&fullname));
-                    },
+                    }
                     _ => {}
                 }
             }
@@ -87,19 +93,27 @@ fn select_path<'a>(value: &'a Value, path: &String) -> Option<&'a Value> {
                 Value::Table(ref table) => {
                     if let Some(found) = table.get(chunk) {
                         current = Some(found);
-                    }
-                    else {
+                    } else {
                         current = None;
+                        break;
                     }
-                },
+                }
+                Value::Array(ref array) => {
+                    if let Ok(index) = chunk.parse::<i32>() {
+
+                    } else {
+
+                    }
+                }
                 _ => {}
             }
+        } else {
+
         }
     }
 
     current
 }
-
 
 fn main() {
     let matches = clap_app!(myapp =>
@@ -110,7 +124,8 @@ fn main() {
         (@arg PATH: +required "Path to TOML file relative to current working directory")
         (@arg QUERY: "Filter is a dot-separated path to a property or category")
         (@arg verbose: -V --verbose "Show debug info")
-    ).get_matches();
+    )
+            .get_matches();
 
     let verbose = matches.is_present("verbose");
     let path = matches.value_of("PATH").unwrap();
@@ -118,7 +133,8 @@ fn main() {
     match File::open(path) {
         Ok(mut file) => {
             let mut content = String::new();
-            file.read_to_string(&mut content).expect("Cannot read file");
+            file.read_to_string(&mut content)
+                .expect("Cannot read file");
 
             match content.parse::<Value>() {
                 Ok(value) => {
@@ -126,28 +142,30 @@ fn main() {
                         if let Some(found) = select_path(&value, &query.to_string()) {
                             print_toml(found, None);
                             println!("");
-                        }
-                        else {
-                            println!("{} Query '{}' not found", "Error:".red(), query.bold().yellow());
+                        } else {
+                            println!("{} Query '{}' not found",
+                                     "Error:".red(),
+                                     query.bold().yellow());
                             std::process::exit(1);
                         }
-                    }
-                    else {
+                    } else {
                         print_toml(&value, None);
                     }
-                },
+                }
                 Err(error) => {
-                    println!("{} Cannot parse file '{}'", "Error:".red(), path.bold().yellow());
+                    println!("{} Cannot parse file '{}'",
+                             "Error:".red(),
+                             path.bold().yellow());
 
-                    if verbose {
-                        println!("{}", error);
-                    }
+                    if verbose { println!("{}", error); }
                     std::process::exit(1);
                 }
             }
-        },
+        }
         Err(error) => {
-            println!("{} Cannot open file '{}'", "Error:".red(), path.bold().yellow());
+            println!("{} Cannot open file '{}'",
+                     "Error:".red(),
+                     path.bold().yellow());
 
             if verbose {
                 println!("{}", error);
